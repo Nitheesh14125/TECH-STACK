fb0a59bb8784b2faee5c9cef824b5e6f04699051
Nitheesh14125 <s.nitheesh14125@gmail.com>
2026-03-18 06:41:48 +0530

    Instance attributes - belong to each object seperately

---
+++ OOP'S - WORK /Instance_attributes.py
@@ -0,0 +1,13 @@
+class student():
+
+    def __init__(self,name,age):
+        self.name = name 
+        self.age = age 
+    
+s1 = student("Nitheesh",20)
+s2 = student("Gagan",26)
+
+print(s1.name,s1.age)
+print(s2.name,s2.age)
+
+    
